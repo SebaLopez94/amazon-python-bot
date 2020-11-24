aeeff486e6f652e875903beaa74599af14f11134
Seba López <sebalg180@gmail.com>
2020-11-24 10:58:27 +0100

    FIX- Buy Now Button Xpath

--- amazon-bot.py
+++ amazon-bot.py
@@ -6,23 +6,26 @@ class PS5Bot():
         self.driver = webdriver.Chrome()
 
     def login(self):
-        self.driver.get('https://www.amazon.es/')
+        self.driver.get('https://www.amazon.com')
         sleep(50)
 
     def checkAndBuyPS5(self):
-        self.driver.get('https://www.amazon.es/dp/B08KKJ37F7?tag=infolinkin-21th=1&psc=1')
+        self.driver.get('THE SHIT YOU WANT TO BUY')
         sleep(1)   
         try:
-            buyNow = self.driver.find_element_by_xpath('//*[@id="buy-now-button"]')
+            buyNow = self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]')
             buyNow.click()
             sleep(2)
-            buyNow2 = self.driver.find_element_by_xpath('//*[@id="submitOrderButtonId"]/span/input')  
+            buyNow2 = self.driver.find_element_by_xpath('//*[@id="hlb-ptc-btn"]')
             buyNow2.click()
+            sleep(2)
+            buyNow3 = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/form/div/div/div/div[2]/div/div[1]/div/div[1]/div/span/span/input')  
+            buyNow3.click()
             sleep(1)
             self.driver.close()
         except Exception as e:
             print(e)
-            sleep(3)
+            sleep(1.5)
             self.checkAndBuyPS5()
 bot = PS5Bot()
 bot.login()
